5f1ff19f85b237125a81b4086b2a06f591a4e2d4
K Hareesh <hareesh.komma@improve10x.com>
2023-06-02 23:45:54 +0530

    we implemented CartProductViewHolder

--- app/src/main/java/com/improve10x/hareeshstorezone/cart/CartProductViewHolder.java
+++ app/src/main/java/com/improve10x/hareeshstorezone/cart/CartProductViewHolder.java
@@ -5,10 +5,13 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.improve10x.hareeshstorezone.databinding.CartProductItemBinding;
+
 public class CartProductViewHolder extends RecyclerView.ViewHolder {
 
-
-    public CartProductViewHolder(@NonNull View itemView) {
-        super(itemView);
+    CartProductItemBinding cartProductItemBinding;
+    public CartProductViewHolder(CartProductItemBinding cartProductItemBinding) {
+        super(cartProductItemBinding.getRoot());
+        this.cartProductItemBinding = cartProductItemBinding;
     }
 }
